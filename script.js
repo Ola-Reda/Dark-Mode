86aeb33fe23959f03496b914d1967d0991007605
Ola-Reda <ola.reda.900@gmail.com>
2024-01-23 14:53:03 +0200

    Add changes

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    document.documentElement.classList.toggle("dark")
+})
 
 const articlesBox = document.querySelector('.articles')
 //display articles function
 function dasplayArticles() {
     let items = '';
     articles.forEach(article => {
-        const dateFormat = moment(article.date).format('MMMM Do, YYYY')
         items += `
             <article>
                 <h2>${article.title}</h2>
                 <div class="info">
-                    <span>${dateFormat}</span>
+                    <span>${article.date}</span>
                     <span>${article.length} min read</span>
                 </div>
                 <p>${article.snippet}</p>
